ba213fc427e174dcd9f843bdcd4d105d6d09e4d0
amonfue <amonfue@arte-consultores.com>
2020-07-20 23:53:54 +0100

    Se adapta el ancho del gráfico al número los elementos del eje X

--- chart.py
+++ chart.py
@@ -9,13 +9,22 @@ class Chart:
         self.y_axis_name = y_axis_name
 
     def generate_chart(self, chart_name):
-        # TODO: Ancho adaptable al contenido -> https://stackoverflow.com/a/54756766
-        chart = plt.figure()
+        figure_width = len(self.x_axis) * 0.15
+        if figure_width < 5:
+            figure_width = 5
 
-        axes = chart.add_axes([0.15, 0.15, 0.75, 0.75])
+        chart = plt.figure(figsize=(figure_width, 5))
+
+        # Ancho adaptable al contenido -> https://stackoverflow.com/a/54756766
+        axes_left = 1 / figure_width
+        axes = chart.add_axes([axes_left, 0.15, 0.98 - axes_left, 0.75])
         axes.set_title(chart_name)
         axes.set_xlabel(self.x_axis_name)
         axes.set_ylabel(self.y_axis_name)
+        axes.grid()
+        axes.tick_params(axis='x', which='major', labelsize=8.5)
+        axes.tick_params(axis='x', labelrotation=90)
+        axes.margins(x=(1 / len(self.x_axis)))
 
         self.set_type_chart(axes)
 
