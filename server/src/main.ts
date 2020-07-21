53a39e02ceb7184c4ea2e3553618a3deb445c732
amonfue <amonfue@arte-consultores.com>
2020-07-21 16:05:20 +0100

    Se cambia el favicon

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
   const configService: ConfigService = app.get(ConfigService);
+  app.use(bodyParser.json({limit: '50mb'}));
   await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
